0011927977cf734ca4aafdbb3742ab4e307f0945
BloodRedTape <gefestroyale@gmail.com>
2022-03-10 12:49:26 +0200

    Utils implemented

---
+++ sources/utils.cpp
@@ -0,0 +1,15 @@
+#include "utils.hpp"
+#include <core/os/file.hpp>
+
+String ReadEntireFile(String filepath) {
+	if(!File::Exist(filepath.Data()))
+		filepath = "../../../" + filepath;
+	SX_ASSERT(File::Exist(filepath.Data()));
+	
+	File file(filepath.Data(), File::Mode::Read);
+	SX_ASSERT(file.IsOpen());
+	
+	String content(file.Size());
+	file.Read(content.Data(), file.Size());
+	return content;
+}
