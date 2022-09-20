763f7ad011a085df377112613300e38f188da337
BloodRedTape <gefestroyale@gmail.com>
2022-09-20 12:33:01 +0300

    cd ../../../

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,10 +3,14 @@
 #include <graphics/api/buffer.hpp>
 #include <graphics/api/gpu.hpp>
 #include "application.hpp"
+#include <core/os/directory.hpp>
+#include <core/env/os.hpp>
 
 int main(){
     GPU::ForceInit();
-
+#ifdef SX_OS_WINDOWS
+    Directory::Change("../../../");
+#endif
     Application().Run();
     return 0;
 }
